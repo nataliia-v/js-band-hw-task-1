c91cb852766e59fb0b44edd51e3d07f915bab316
nataliia-v <nataly.verbenskaya@gmail.com>
2019-11-14 14:53:18 +0200

    build

--- build/js/index.js
+++ build/js/index.js
@@ -1,14 +1,14 @@
 !(function(e) {
   var t = {};
-  function n(r) {
-    if (t[r]) return t[r].exports;
-    var a = (t[r] = { i: r, l: !1, exports: {} });
-    return e[r].call(a.exports, a, a.exports, n), (a.l = !0), a.exports;
+  function n(a) {
+    if (t[a]) return t[a].exports;
+    var r = (t[a] = { i: a, l: !1, exports: {} });
+    return e[a].call(r.exports, r, r.exports, n), (r.l = !0), r.exports;
   }
   (n.m = e),
     (n.c = t),
-    (n.d = function(e, t, r) {
-      n.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: r });
+    (n.d = function(e, t, a) {
+      n.o(e, t) || Object.defineProperty(e, t, { enumerable: !0, get: a });
     }),
     (n.r = function(e) {
       'undefined' != typeof Symbol &&
@@ -19,21 +19,21 @@
     (n.t = function(e, t) {
       if ((1 & t && (e = n(e)), 8 & t)) return e;
       if (4 & t && 'object' == typeof e && e && e.__esModule) return e;
-      var r = Object.create(null);
+      var a = Object.create(null);
       if (
-        (n.r(r),
-        Object.defineProperty(r, 'default', { enumerable: !0, value: e }),
+        (n.r(a),
+        Object.defineProperty(a, 'default', { enumerable: !0, value: e }),
         2 & t && 'string' != typeof e)
       )
-        for (var a in e)
+        for (var r in e)
           n.d(
-            r,
             a,
+            r,
             function(t) {
               return e[t];
-            }.bind(null, a),
+            }.bind(null, r),
           );
-      return r;
+      return a;
     }),
     (n.n = function(e) {
       var t =
@@ -61,57 +61,57 @@
         'function' == typeof window.msCrypto.getRandomValues &&
         msCrypto.getRandomValues.bind(msCrypto));
     if (n) {
-      var r = new Uint8Array(16);
+      var a = new Uint8Array(16);
       e.exports = function() {
-        return n(r), r;
+        return n(a), a;
       };
     } else {
-      var a = new Array(16);
+      var r = new Array(16);
       e.exports = function() {
         for (var e, t = 0; t < 16; t++)
-          0 == (3 & t) && (e = 4294967296 * Math.random()), (a[t] = (e >>> ((3 & t) << 3)) & 255);
-        return a;
+          0 == (3 & t) && (e = 4294967296 * Math.random()), (r[t] = (e >>> ((3 & t) << 3)) & 255);
+        return r;
       };
     }
   },
   function(e, t) {
-    for (var n = [], r = 0; r < 256; ++r) n[r] = (r + 256).toString(16).substr(1);
+    for (var n = [], a = 0; a < 256; ++a) n[a] = (a + 256).toString(16).substr(1);
     e.exports = function(e, t) {
-      var r = t || 0,
-        a = n;
+      var a = t || 0,
+        r = n;
       return [
-        a[e[r++]],
-        a[e[r++]],
-        a[e[r++]],
-        a[e[r++]],
+        r[e[a++]],
+        r[e[a++]],
+        r[e[a++]],
+        r[e[a++]],
         '-',
-        a[e[r++]],
-        a[e[r++]],
+        r[e[a++]],
+        r[e[a++]],
         '-',
-        a[e[r++]],
-        a[e[r++]],
+        r[e[a++]],
+        r[e[a++]],
         '-',
-        a[e[r++]],
-        a[e[r++]],
+        r[e[a++]],
+        r[e[a++]],
         '-',
-        a[e[r++]],
-        a[e[r++]],
-        a[e[r++]],
-        a[e[r++]],
-        a[e[r++]],
-        a[e[r++]],
+        r[e[a++]],
+        r[e[a++]],
+        r[e[a++]],
+        r[e[a++]],
+        r[e[a++]],
+        r[e[a++]],
       ].join('');
     };
   },
   function(e, t, n) {
-    var r = n(3),
-      a = n(4),
-      o = a;
-    (o.v1 = r), (o.v4 = a), (e.exports = o);
+    var a = n(3),
+      r = n(4),
+      o = r;
+    (o.v1 = a), (o.v4 = r), (e.exports = o);
   },
   function(e, t, n) {
-    var r,
-      a,
+    var a,
+      r,
       o = n(0),
       c = n(1),
       s = 0,
@@ -119,12 +119,12 @@
     e.exports = function(e, t, n) {
       var i = (t && n) || 0,
         l = t || [],
-        u = (e = e || {}).node || r,
-        p = void 0 !== e.clockseq ? e.clockseq : a;
+        u = (e = e || {}).node || a,
+        p = void 0 !== e.clockseq ? e.clockseq : r;
       if (null == u || null == p) {
         var m = o();
-        null == u && (u = r = [1 | m[0], m[1], m[2], m[3], m[4], m[5]]),
-          null == p && (p = a = 16383 & ((m[6] << 8) | m[7]));
+        null == u && (u = a = [1 | m[0], m[1], m[2], m[3], m[4], m[5]]),
+          null == p && (p = r = 16383 & ((m[6] << 8) | m[7]));
       }
       var f = void 0 !== e.msecs ? e.msecs : new Date().getTime(),
         v = void 0 !== e.nsecs ? e.nsecs : d + 1,
@@ -135,7 +135,7 @@
         v >= 1e4)
       )
         throw new Error("uuid.v1(): Can't create more than 10M uuids/sec");
-      (s = f), (d = v), (a = p);
+      (s = f), (d = v), (r = p);
       var h = (1e4 * (268435455 & (f += 122192928e5)) + v) % 4294967296;
       (l[i++] = (h >>> 24) & 255),
         (l[i++] = (h >>> 16) & 255),
@@ -153,83 +153,87 @@
     };
   },
   function(e, t, n) {
-    var r = n(0),
-      a = n(1);
+    var a = n(0),
+      r = n(1);
     e.exports = function(e, t, n) {
       var o = (t && n) || 0;
       'string' == typeof e && ((t = 'binary' === e ? new Array(16) : null), (e = null));
-      var c = (e = e || {}).random || (e.rng || r)();
+      var c = (e = e || {}).random || (e.rng || a)();
       if (((c[6] = (15 & c[6]) | 64), (c[8] = (63 & c[8]) | 128), t))
         for (var s = 0; s < 16; ++s) t[o + s] = c[s];
-      return t || a(c);
+      return t || r(c);
     };
   },
   function(e, t, n) {
     'use strict';
     n.r(t);
     n(6), n(11);
-    var r = (e, t, n, r) => {
-        const a = document.createElement('div');
-        a.classList.add('formRoot');
+    var a = (e, t, n, a) => {
+        const r = document.createElement('div');
+        r.classList.add('formRoot');
         const o = document.createElement('form'),
           c = document.createElement('button');
-        return (
-          (c.textContent = 'Save'),
+        (c.textContent = 'Save'),
           (c.type = 'submit'),
           o.addEventListener('submit', n => {
             n.preventDefault();
-            const a = n.target.getElementsByClassName('formField-js'),
+            const r = n.target.getElementsByClassName('formField-js'),
               o = {};
-            Array.from(a).forEach(e => {
+            Array.from(r).forEach(e => {
               (o[e.name] = e.value), (e.value = '');
             });
             const c = JSON.parse(localStorage.getItem(e)),
               s = new t(o),
               d = JSON.stringify(c ? [...c, s] : [s]);
-            localStorage.setItem(e, d), r && r();
-          }),
+            localStorage.setItem(e, d), a && a();
+          });
+        const s = document.createElement('h3');
+        return (
+          (s.textContent = e),
+          (s.className = 'form-title'),
           n.forEach(({ name: e, type: t, options: n }) => {
-            const r = document.createElement('div');
-            r.classList.add('fieldRoot');
-            const s = document.createElement('label');
-            let d;
-            switch (((s.textContent = e), t)) {
+            const a = document.createElement('div');
+            a.classList.add('fieldRoot');
+            const d = document.createElement('label');
+            let i;
+            switch (((d.textContent = e), t)) {
               case 'text':
               case 'number':
-                (d = document.createElement('input')).setAttribute('name', e),
-                  d.setAttribute('type', t);
+                (i = document.createElement('input')).setAttribute('name', e),
+                  i.setAttribute('type', t);
                 break;
               case 'select':
-                (d = document.createElement('select')).setAttribute('name', e),
+                (i = document.createElement('select')).setAttribute('name', e),
                   n.forEach(e => {
                     const t = document.createElement('option');
-                    (t.value = e), (t.textContent = e), d.appendChild(t);
+                    (t.value = e), (t.textContent = e), i.appendChild(t);
                   });
                 break;
               default:
-                d = null;
+                i = null;
             }
-            d.classList.add('formField-js'),
-              (d.required = !0),
-              s.appendChild(d),
+            i.classList.add('formField-js'),
+              (i.required = !0),
               r.appendChild(s),
-              o.appendChild(r),
+              d.appendChild(i),
+              a.appendChild(d),
+              o.appendChild(a),
               o.appendChild(c),
-              a.appendChild(o);
+              r.appendChild(o);
           }),
-          a
+          r
         );
       },
-      a = (e, t, n) => {
-        const r = document.getElementById('root'),
-          a = document.createElement('div');
+      r = (e, t, n) => {
+        const a = document.getElementById('root'),
+          r = document.createElement('div');
         return (
-          (a.id = n),
-          r.appendChild(a),
+          (r.id = n),
+          a.appendChild(r),
           () => {
             const n = JSON.parse(localStorage.getItem(t)),
-              r = e(n);
-            (a.innerHTML = ''), a.appendChild(r);
+              a = e(n);
+            (r.innerHTML = ''), r.appendChild(a);
           }
         );
       };
@@ -255,12 +259,12 @@
       },
       i = n(2);
     var l = class {
-      constructor({ model: e, producedYear: t, capacity: n, averageSpeed: r }) {
+      constructor({ model: e, producedYear: t, capacity: n, averageSpeed: a }) {
         (this.id = Object(i.v4)()),
           (this.model = e),
           (this.producedYear = t),
           (this.capacity = n),
-          (this.averageSpeed = r);
+          (this.averageSpeed = a);
       }
       showCapacityInPounds() {
         return 2.20462 * this.capacity;
@@ -271,16 +275,16 @@
         model: e,
         producedYear: t,
         capacity: n,
-        averageSpeed: r,
-        name: a,
+        averageSpeed: a,
+        name: r,
         countOfTeam: o,
       }) {
-        super({ model: e, producedYear: t, capacity: n, averageSpeed: r }),
-          (this.name = a),
+        super({ model: e, producedYear: t, capacity: n, averageSpeed: a }),
+          (this.name = r),
           (this.countOfTeam = o);
       }
       showAverageSpeed() {
-        alert(`${this.averageSpeed}nm`);
+        console.log(`${this.averageSpeed}nm`);
       }
     };
     var p = class extends l {
@@ -288,12 +292,12 @@
           model: e,
           producedYear: t,
           capacity: n,
-          averageSpeed: r,
-          licensePlate: a,
+          averageSpeed: a,
+          licensePlate: r,
           typeOfGas: o,
         }) {
-          super({ model: e, producedYear: t, capacity: n, averageSpeed: r }),
-            (this.licensePlate = a),
+          super({ model: e, producedYear: t, capacity: n, averageSpeed: a }),
+            (this.licensePlate = r),
             (this.typeOfGas = o);
         }
         showAverageSpeed() {
@@ -303,26 +307,26 @@
       m = (e, t) => {
         const n = document.createElement('table');
         n.classList.add('table');
-        const r = document.createElement('caption');
-        r.textContent = e;
-        const a = document.createElement('thead'),
+        const a = document.createElement('caption');
+        a.textContent = e;
+        const r = document.createElement('thead'),
           o = document.createElement('tbody');
         return (
           o.classList.add('tbody'),
           t.forEach(e => {
             const t = document.createElement('th');
-            (t.textContent = e), a.appendChild(t);
+            (t.textContent = e), r.appendChild(t);
           }),
-          n.appendChild(r),
           n.appendChild(a),
+          n.appendChild(r),
           n.appendChild(o),
           e => {
             if (((o.innerHTML = ''), e))
               e.forEach(e => {
                 const n = document.createElement('tr');
                 t.forEach(t => {
-                  const r = document.createElement('td');
-                  r.classList.add('td'), (r.textContent = e[t]), n.appendChild(r);
+                  const a = document.createElement('td');
+                  a.classList.add('td'), (a.textContent = e[t]), n.appendChild(a);
                 }),
                   o.appendChild(n);
               });
@@ -342,16 +346,16 @@
       C = ['id', ...y.map(e => e.name)],
       g = ['id', ...h.map(e => e.name)],
       b = s.map(e => e.name),
-      E = a(m('Ships List', C), 'ships'),
-      S = a(m('Truck list', g), 'trucks'),
-      x = a(m('Costs Of Delivery', b), 'costsOfDelivery'),
+      E = r(m('Ships List', C), 'ships'),
+      S = r(m('Truck list', g), 'trucks'),
+      x = r(m('Costs Of Delivery', b), 'costsOfDelivery'),
       O = (() => {
         const e = document.createElement('div');
         return (
           (e.className = 'costOfDeliveryContainer'),
           document.querySelector('.formsContainer').appendChild(e),
           t => {
-            const n = r(
+            const n = a(
               'costsOfDelivery',
               d,
               (() => {
@@ -370,10 +374,10 @@
           }
         );
       })(),
-      w = r('ships', u, y, () => {
+      w = a('ships', u, y, () => {
         E(), O(x);
       }),
-      j = r('trucks', p, h, () => {
+      j = a('trucks', p, h, () => {
         S(), O(x);
       });
     v.appendChild(w), v.appendChild(j), O(), E(), S(), x();
